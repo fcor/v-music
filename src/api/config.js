1cd969b2255073e244b6948ba86aa8fb3f9ffe98
Fabio <fcor14@hotmail.com>
2017-06-11 10:11:10 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '41b6df4ed1333a48bf4edff05bf8fde2',
+  secret: 'a93b943069e911d3538582aa206170bc',
+  registeredTo: 'fcor14'
+}
+
+// Application name	platzimusic
+// API key	41b6df4ed1333a48bf4edff05bf8fde2
+// Shared secret	a93b943069e911d3538582aa206170bc
+// Registered to	fcor14
